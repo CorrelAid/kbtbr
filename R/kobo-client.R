45a87236217dec570e0f3f7dcd02d1c5faed4725
Frie <fr1e@pm.me>
2021-05-02 23:03:46 +0200

    kobo class and koboclient class with get

--- R/kobo-client.R
+++ R/kobo-client.R
@@ -2,22 +2,20 @@
 #' @description a class to interact with the KoboToolbox API
 KoboClient <- R6::R6Class(
     "KoboClient",
-    private = list(kobo_token = "",
-                   .base_url = NULL),
+    private = list(
+        base_url = "",
+        kobo_token = ""
+    ), 
     public = list(
-        #' @field v1_enabled boolean. whether or not the API v1 was enabled for this client by passing in an base URL
-        v1_enabled = FALSE,
         #' @param base_url character. The full base URL of the API version 2 (known as /api/v2). For example: https://kobo.correlaid.org/api/v2.
         #' @param kobo_token character. The API token. Defaults to NULL.
-        #' @param base_url_v1 character. The base URL of the API of your KoBoCAT API (also known as /api/v1). Defaults to NULL. For example: https://kc.correlaid.org/api/v1/.
         initialize = function(base_url,
-                              kobo_token = NULL,
-                              base_url_v1 = NULL) {
-            private$.base_url <- base_url
+                              kobo_token = Sys.getenv("KBTBR_TOKEN")) {
+            
+            private$base_url <- base_url
 
             # set the token internally
-            if (checkmate::test_null(kobo_token) &
-                Sys.getenv("KBTBR_TOKEN") == "") {
+            if (Sys.getenv("KBTBR_TOKEN") == "") {
                 usethis::ui_stop(
                     "No valid token detected. Set the KBTBR_TOKEN environment variable or pass the token directly to the function (not recommended)."
                 )
@@ -25,23 +23,16 @@ KoboClient <- R6::R6Class(
             # check that the token is a character
             checkmate::assert_character(kobo_token)
             private$kobo_token <- kobo_token
+            super$initialize(url = base_url, headers = list(Authorization = paste0("Token ", kobo_token)))
+        },
 
-            # if specified, set the base url for api/v1 internally
-            if (!checkmate::test_null(base_url_v1)) {
-                checkmate::assert_character(base_url_v1)
-                self$v1_enabled <- TRUE
-            }
-        }
-    ),
-    active = list(
-        #' @field base_url
-        #' read-only
-        base_url = function(value) {
-            if (!missing(value)) {
-                usethis::ui_stop("You can't set base_url after initialization.")
-            } else {
-                return(private$.base_url)
-            }
+        # TODO: understand ... 
+        get = function(path, query = list(), ...) {
+            res <- super$get(path = path, query = query, ...)
+            res$raise_for_status()
+            res$raise_for_ct_json()
+            res$parse("UTF-8") %>% 
+                jsonlite::fromJSON()
         }
     ),
     inherit = crul::HttpClient
--- tests/testthat/test-kobo-client-class.R
+++
@@ -1,6 +0,0 @@
-test_that("the class can be initalized", {
-    base_url <- "https://kobo.correlaid.org/api/v2"
-    kobo <- KoboClient$new(base_url = base_url, kobo_token = "faketoken")
-    expect_false(kobo$v1_enabled)
-    expect_equal(kobo$base_url, base_url)
-})
