130d163f78b114dc6345b9fb3740d168c879633c
Malte Kyhos <mkyhos@protonmail.com>
2021-05-03 22:22:11 +0200

    Extend documentation: KoboClient
    - Add some WIP descriptions for any method, and parameter
    - Add export statement
    - Unfortunately, @inheritParams does not seem to work well with
    superclasses in R6.... yet!
    See https://github.com/r-lib/roxygen2/issues/996

--- R/kobo-client.R
+++ R/kobo-client.R
@@ -1,14 +1,18 @@
-#' KoboClient
-#' @description a class to interact with the KoboToolbox API
-KoboClient <- R6::R6Class(
-    "KoboClient",
+#' @title KoboClient
+#' @description 
+#' A class to interact with the KoboToolbox API, extending `crul::HttpClient`.
+#' @seealso \code{\link[crul]{HttpClient}}
+#' @export
     private = list(
         base_url = "",
         kobo_token = ""
     ), 
     public = list(
-        #' @param base_url character. The full base URL of the API version 2 (known as /api/v2). For example: https://kobo.correlaid.org/api/v2.
-        #' @param kobo_token character. The API token. Defaults to NULL.
+        #' @description
+        #' Initialization method for class "KoboClient".
+        #' @param base_url character. The full base URL of the API.
+        #' @param kobo_token character. The API token. Defaults to a request to
+        #'  the system environment.
         initialize = function(base_url,
                               kobo_token = Sys.getenv("KBTBR_TOKEN")) {
             
@@ -25,8 +29,12 @@ KoboClient <- R6::R6Class(
             private$kobo_token <- kobo_token
             super$initialize(url = base_url, headers = list(Authorization = paste0("Token ", kobo_token)))
         },
-
-        # TODO: understand ... 
+        #' @description #TODO Foster further understanding. 
+        #' @param path character. Path component of the endpoint. 
+        #' @param query list. A named list which is parsed to the query
+        #'  component. The order is not hierarchical.
+        #' @param ... crul-options. Additional option arguments, see
+        #'  crul::HttpClient for reference
         get = function(path, query = list(), ...) {
             res <- super$get(path = path, query = query, ...)
             res$raise_for_status()
