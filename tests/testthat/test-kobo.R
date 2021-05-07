7293d5c7b56826120a5a6bee71c80bcccd163337
Malte Kyhos <mkyhos@protonmail.com>
2021-05-08 00:03:26 +0200

    Adding first tests for Kobo class

--- tests/testthat/test-kobo.R
+++ tests/testthat/test-kobo.R
@@ -1,7 +1,55 @@
-test_that("the class can be initalized", {
-    base_url <- "https://kobo.correlaid.org"
-    kobo <- Kobo$new(base_url_v2 = base_url, kobo_token = Sys.getenv("KBTBR_TOKEN"))
-    assets <- kobo$get_assets()
-    expect_true(is.list(assets))
-    str(assets, 1)
+base_url <- "https://kobo.correlaid.org"
+
+
+
+#' -----------------------------------------------------------------------------
+#' Testing basic properties, construction
+
+test_that("Construction & token passing works", {
+
+    # Case: No token is set in envvar. Testing argument:
+    withr::with_envvar(
+        new = c("KBTBR_TOKEN" = ""),
+        code = {
+
+            # a) no `kobo_token` is provided, is the error from 
+            #    KoboClient propagated correctly?
+            expect_error(
+                object = Kobo$new(base_url_v2 = base_url),
+                regexp = "No valid token detected."
+                )
+
+            # b) `kobo_token` argument is provided, supersedes env var. 
+            #    is this propagated correctly to KoboClient?
+            kobo_obj <- Kobo$new(base_url_v2 = base_url, kobo_token = "foo")
+            expect_identical(
+                class(kobo_obj),
+                c("Kobo", "R6")
+            )
+        }
+    )
+
 })
+
+#' -----------------------------------------------------------------------------
+#' Testing $get_* methods
+
+test_that("Requests with faulty token throw error", {
+    # Case: Faulty token is via envvar, get request fails
+    #TODO Replace this with http / vcr testing etc
+    #TODO Discuss: should this only be tested within KoboClient?
+    #TODO Discuss: should we think about custom errors for http stuff? -> probably not
+    withr::with_envvar(
+        new = c("KBTBR_TOKEN" = "foo"),
+        code = {
+            kobo <- Kobo$new(base_url_v2 = base_url)
+            expect_error(kobo$get_assets(), regexp = "403")
+        }
+    )
+
+
+})
+
+
+#' -----------------------------------------------------------------------------
+#' Testing `$post_*`` methods
