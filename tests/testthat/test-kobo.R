69ee2b09e159611905c5c83495d72f4b1a063973
Frie <fr1e@pm.me>
2021-05-13 17:03:00 +0200

    regexp without host name

--- tests/testthat/test-kobo.R
+++ tests/testthat/test-kobo.R
@@ -35,6 +35,6 @@ vcr::use_cassette("kobo-get-404", {
 
 test_that("non-existing kobo host throws error", {
         kobo <- Kobo$new(base_url_v2 = "https://nokobohere.correlaid.org", kobo_token = Sys.getenv("KBTBR_TOKEN"))
-        expect_error(kobo$get("assets/"), regexp = ".+?host.+nokobohere.correlaid.org.+?")
+        expect_error(kobo$get("assets/"), regexp = "^SSL.+certificate.+")
 })
 
