2be6f1079a6c53ccd1c223286186f0c2152b76eb
Frie <fr1e@pm.me>
2021-06-06 21:47:56 +0200

    Housekeeping #18 run styler::style_pkg()

--- R/kobo-client.R
+++ R/kobo-client.R
@@ -6,60 +6,60 @@
 #' @importFrom usethis ui_stop
 #' @export
 KoboClient <- R6::R6Class("KoboClient",
-    inherit = crul::HttpClient,
-    private = list(
-        base_url = "",
-        kobo_token = ""
-    ), 
-    public = list(
-        #' @description
-        #' Initialization method for class "KoboClient".
-        #' @param base_url character. The full base URL of the API.
-        #' @param kobo_token character. The API token. Defaults to requesting
-        #'  the system environment variable `KBTBR_TOKEN`.
-        initialize = function(base_url,
-                              kobo_token = Sys.getenv("KBTBR_TOKEN")) {
-            
-            # Check and set private fields
-            checkmate::assert_character(kobo_token)
-            if (kobo_token == "") {
-                usethis::ui_stop(
-                    "No valid token detected. Set the KBTBR_TOKEN environment
+  inherit = crul::HttpClient,
+  private = list(
+    base_url = "",
+    kobo_token = ""
+  ),
+  public = list(
+    #' @description
+    #' Initialization method for class "KoboClient".
+    #' @param base_url character. The full base URL of the API.
+    #' @param kobo_token character. The API token. Defaults to requesting
+    #'  the system environment variable `KBTBR_TOKEN`.
+    initialize = function(base_url,
+                          kobo_token = Sys.getenv("KBTBR_TOKEN")) {
+
+      # Check and set private fields
+      checkmate::assert_character(kobo_token)
+      if (kobo_token == "") {
+        usethis::ui_stop(
+          "No valid token detected. Set the KBTBR_TOKEN environment
                     variable or pass the token directly to the function
                     (not recommended)."
-                )
-            }
-            private$kobo_token <- kobo_token
-            private$base_url <- base_url
+        )
+      }
+      private$kobo_token <- kobo_token
+      private$base_url <- base_url
 
-            super$initialize(
-                url = base_url,
-                headers = list(
-                    Authorization = paste0("Token ", kobo_token)
-                    )
-                )
-        },
-        #' @description
-        #' Perform a GET request (with additional checks)
-        #'
-        #' @details
-        #' Extension of the `crul::HttpClient$get()` method that checks
-        #' the HttpResponse object on status, that it is of type
-        #' `application/json`, and parses the response text subsequently from
-        #' JSON to R list representation.
-        #' @param path character. Path component of the endpoint. 
-        #' @param query list. A named list which is parsed to the query
-        #'  component. The order is not hierarchical.
-        #' @param ... crul-options. Additional option arguments, see
-        #'  [`crul::HttpClient`] for reference
-        #' @return Returns a list, parsed from the HttpResponse JSON object.
-        get = function(path, query = list(), ...) {
-            res <- super$get(path = path, query = query, ...)
-            # Perform additional checks, json to list parsing
-            res$raise_for_status()
-            res$raise_for_ct_json()
-            res$parse("UTF-8") %>%
-                jsonlite::fromJSON()
-        }
-    )
+      super$initialize(
+        url = base_url,
+        headers = list(
+          Authorization = paste0("Token ", kobo_token)
+        )
+      )
+    },
+    #' @description
+    #' Perform a GET request (with additional checks)
+    #'
+    #' @details
+    #' Extension of the `crul::HttpClient$get()` method that checks
+    #' the HttpResponse object on status, that it is of type
+    #' `application/json`, and parses the response text subsequently from
+    #' JSON to R list representation.
+    #' @param path character. Path component of the endpoint.
+    #' @param query list. A named list which is parsed to the query
+    #'  component. The order is not hierarchical.
+    #' @param ... crul-options. Additional option arguments, see
+    #'  [`crul::HttpClient`] for reference
+    #' @return Returns a list, parsed from the HttpResponse JSON object.
+    get = function(path, query = list(), ...) {
+      res <- super$get(path = path, query = query, ...)
+      # Perform additional checks, json to list parsing
+      res$raise_for_status()
+      res$raise_for_ct_json()
+      res$parse("UTF-8") %>%
+        jsonlite::fromJSON()
+    }
+  )
 )
--- tests/testthat/test-kobo-client.R
+++ tests/testthat/test-kobo-client.R
@@ -3,33 +3,33 @@ BASE_URL <- "https://kobo.correlaid.org"
 #' Testing $get_* methods
 
 vcr::use_cassette("kobo-client-403", {
-    test_that("Requests with faulty token throw error", {
-        # Case: Faulty token is via envvar, get request fails
-        withr::with_envvar(
-            new = c("KBTBR_TOKEN" = "foo"),
-            code = {
-                kc <- KoboClient$new(base_url = BASE_URL)
-                expect_error(kc$get("api/v2/assets/"), regexp = "403")
-            }
-        )
-    })
+  test_that("Requests with faulty token throw error", {
+    # Case: Faulty token is via envvar, get request fails
+    withr::with_envvar(
+      new = c("KBTBR_TOKEN" = "foo"),
+      code = {
+        kc <- KoboClient$new(base_url = BASE_URL)
+        expect_error(kc$get("api/v2/assets/"), regexp = "403")
+      }
+    )
+  })
 })
 
 test_that("Kobo Client can fetch assets", {
-    vcr::use_cassette("kobo-client-get-assets-simple-get", {
-        koboclient <- KoboClient$new(base_url = BASE_URL, kobo_token = Sys.getenv("KBTBR_TOKEN"))
-        assets <- koboclient$get("api/v2/assets/", query = list(format = "json")) # trailing slash again!
-    })
-    expect_setequal(names(assets), c("count", "next", "previous", "results"))
-    expect_true(all(c("url", "owner", "kind", "name", "asset_type") %in% colnames(assets$results)))
-    expect_equal(nrow(assets$results), 8)
-    expect_equal(assets$count, 8)
+  vcr::use_cassette("kobo-client-get-assets-simple-get", {
+    koboclient <- KoboClient$new(base_url = BASE_URL, kobo_token = Sys.getenv("KBTBR_TOKEN"))
+    assets <- koboclient$get("api/v2/assets/", query = list(format = "json")) # trailing slash again!
+  })
+  expect_setequal(names(assets), c("count", "next", "previous", "results"))
+  expect_true(all(c("url", "owner", "kind", "name", "asset_type") %in% colnames(assets$results)))
+  expect_equal(nrow(assets$results), 8)
+  expect_equal(assets$count, 8)
 })
 
 
 vcr::use_cassette("kobo-client-get-404", {
-    test_that("non existing route throws 404 error", {
-        koboclient <- KoboClient$new(base_url = BASE_URL, kobo_token = Sys.getenv("KBTBR_TOKEN"))
-        expect_error(koboclient$get("api/v2/doesnotexist/"), regexp = "404")
-    })
+  test_that("non existing route throws 404 error", {
+    koboclient <- KoboClient$new(base_url = BASE_URL, kobo_token = Sys.getenv("KBTBR_TOKEN"))
+    expect_error(koboclient$get("api/v2/doesnotexist/"), regexp = "404")
+  })
 })
