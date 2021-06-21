3f15a4b2ebd0a4bcf479146523fb66d025efc9fc
Frie <fr1e@pm.me>
2021-06-21 20:53:16 +0200

    #49 indent with 4 spaces instead of 2

--- tests/testthat/setup-kbtbr.R
+++ tests/testthat/setup-kbtbr.R
@@ -3,19 +3,19 @@ library("vcr") # *Required* as vcr is set up on loading
 vcr_dir <- vcr::vcr_test_path("fixtures")
 
 if (!nzchar(Sys.getenv("KBTBR_TOKEN"))) {
-  if (dir.exists(vcr_dir)) {
-    # Fake API token to fool our package
-    Sys.setenv("KBTBR_TOKEN" = "fakebearertoken")
-  } else {
-    # If there's no mock files nor API token, impossible to run tests
-    stop("No API key nor cassettes, tests cannot be run.",
-      call. = FALSE
-    )
-  }
+    if (dir.exists(vcr_dir)) {
+        # Fake API token to fool our package
+        Sys.setenv("KBTBR_TOKEN" = "fakebearertoken")
+    } else {
+        # If there's no mock files nor API token, impossible to run tests
+        stop("No API key nor cassettes, tests cannot be run.",
+            call. = FALSE
+        )
+    }
 }
 
 invisible(vcr::vcr_configure(
-  dir = vcr_dir,
-  filter_request_headers = list(Authorization = "fakebearertoken")
+    dir = vcr_dir,
+    filter_request_headers = list(Authorization = "fakebearertoken")
 ))
 vcr::check_cassette_names()
