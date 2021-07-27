1d794653ec87d82598aa496a413313b1b461e568
Malte Kyhos <mkyhos@protonmail.com>
2021-07-27 22:37:32 +0200

    [#26] Add check_repair_path function

--- R/utils.R
+++ R/utils.R
@@ -2,3 +2,15 @@
 # url builder (take base url, asset type and identifier and output format?)
 # if we write the functions outside the classes and set them later in the class
 # code readability is higher imo
+
+
+#' @title Check URL
+#' @param path The path, for example appended to the base URL.
+#' @return The path string with optionally an appended trailing slash.
+check_repair_path <- function(path) {
+    if (substr(path, nchar(path), nchar(path)) != "/") {
+        return(paste0(path, "/"))
+    } else {
+        return(path)
+    }
+}
