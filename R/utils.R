4b5cb5da8142b3f43c753effef68c12086a1298a
Malte Kyhos <mkyhos@protonmail.com>
2021-08-08 23:27:50 +0200

    [#26] Enforce trailing slashes for `POST` and `GET` (#54)
    * [#26] Add trailing slash to post and get requests
    
    * [#26] Add check_repair_path function
    
    * Add test for function
    
    * Set noRd tag for internal function

--- R/utils.R
+++ R/utils.R
@@ -3,6 +3,18 @@
 # if we write the functions outside the classes and set them later in the class
 # code readability is higher imo
 
+#' @title Check URL
+#' @param path The path, for example appended to the base URL.
+#' @return The path string with optionally an appended trailing slash.
+#' @noRd
+check_repair_path <- function(path) {
+    if (substr(path, nchar(path), nchar(path)) != "/") {
+        return(paste0(path, "/"))
+    } else {
+        return(path)
+    }
+}
+
 #' @description
 #' Converts R list into JSON-like string.
 #'
@@ -10,7 +22,6 @@
 #'
 #' @param list R list to be converted.
 #' @return JSON-like string
-
 list_as_json_char <- function(list) {
     jsonlite::toJSON(x = list, pretty = TRUE, auto_unbox = TRUE) %>%
         as.character()
