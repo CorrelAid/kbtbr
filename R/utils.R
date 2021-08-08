f4eca94b87e9f1545e4547515d97299895c7ee53
Malte Kyhos <mkyhos@protonmail.com>
2021-08-08 22:49:18 +0200

    Add infix coalesce operator

--- R/utils.R
+++ R/utils.R
@@ -2,3 +2,11 @@
 # url builder (take base url, asset type and identifier and output format?)
 # if we write the functions outside the classes and set them later in the class
 # code readability is higher imo
+
+#' @title Infix Coalesce Operator
+#' @describtion Makes coalesce chainable, as often used in
+#' tidyverse packages.
+#' @noRd 
+`%||%` <- function(x, y) {
+  if (is.null(x)) y else x
+}
